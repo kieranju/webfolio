106013c881abe7b6c50ee717460a9173b1e0408b
Kieran Utz <Kieranju@gmail.com>
2015-07-02 17:45:35 -0700

    Mobile fixes, transition adjustments

--- development/js/main.js
+++ development/js/main.js
@@ -6,7 +6,7 @@ function addClass(_className) {
 
 function disableHover() {
     clearTimeout(scrollTimer);
-    
+
     $(document.body).addClass("disable-hover");
     scrollTimer = setTimeout(function() {
         $(document.body).removeClass("disable-hover");
@@ -16,7 +16,7 @@ function disableHover() {
 function inView(_className, _callback) {
     var args = Array.prototype.slice.call(arguments);
     args.splice(0,2);
-    
+
     $(_className).each(function(i) {
         var objectBottom = $(this).position().top + $(this).outerHeight();
         var windowBottom = $(window).scrollTop() + $(window).height();
@@ -28,14 +28,14 @@ function inView(_className, _callback) {
 }
 
 // initial elements on screen
-inView(".panel", addClass, "panel-view");
+// inView(".panel", addClass, "panel-view");
 
 $(window).scroll(function() {
-    inView(".panel", addClass, "panel-view");
+    // inView(".panel", addClass, "panel-view");
     disableHover();
 });
 
 $(window).resize(function() {
-    inView(".panel", addClass, "panel-view");
+    // inView(".panel", addClass, "panel-view");
     disableHover();
 });
